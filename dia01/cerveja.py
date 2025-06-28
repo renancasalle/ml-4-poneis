37d929abe347547a3848c6aebe018bf8a20b84da
renancasalle <renancasalle@gmail.com>
2025-06-28 15:34:20 -0300

    chore: remove unused data files and scripts

--- dia01/cerveja.py
+++
@@ -1,8 +0,0 @@
-# %%
-import pandas as pd
-
-df = pd.read_excel("../data/dados_cerveja.xlsx")
-df
-
-# %%
-## Como podemos fazer a máquina aprender?
--- dia01/frutas.py
+++
@@ -1,6 +0,0 @@
-# %%
-import pandas as pd
-
-df = pd.read_excel("../data/dados_frutas.xlsx")
-df
-
